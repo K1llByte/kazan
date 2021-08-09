fb01210a202fdec07e14c14fb262f4bd546c76de
KillByte <killbyte.prototype@gmail.com>
2021-08-10 00:09:41 +0100

    Updated vulkan debug callback to print only warnings and errors

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -204,7 +204,7 @@ void Renderer::cleanup()
     glfwTerminate();
 }
 
-void Renderer::framebuffer_resize_callback(GLFWwindow* window, int width, int height)
+void Renderer::framebuffer_resize_callback(GLFWwindow* window, int /* width */, int /* height */)
 {
     auto app = reinterpret_cast<Renderer*>(glfwGetWindowUserPointer(window));
     app->m_framebuffer_resized = true;
@@ -337,12 +337,15 @@ void Renderer::populate_debug_messenger_create_info(
     VkDebugUtilsMessengerCreateInfoEXT& create_info)
 {
     create_info.sType =           VK_STRUCTURE_TYPE_DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT;
-    create_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT
+    create_info.messageSeverity = VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT
                                 | VK_DEBUG_UTILS_MESSAGE_SEVERITY_WARNING_BIT_EXT
-                                | VK_DEBUG_UTILS_MESSAGE_SEVERITY_ERROR_BIT_EXT;
+                                // | VK_DEBUG_UTILS_MESSAGE_SEVERITY_INFO_BIT_EXT
+                                // | VK_DEBUG_UTILS_MESSAGE_SEVERITY_VERBOSE_BIT_EXT
+                                ;
     create_info.messageType =     VK_DEBUG_UTILS_MESSAGE_TYPE_GENERAL_BIT_EXT
                                 | VK_DEBUG_UTILS_MESSAGE_TYPE_VALIDATION_BIT_EXT
-                                | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT;
+                                | VK_DEBUG_UTILS_MESSAGE_TYPE_PERFORMANCE_BIT_EXT
+                                ;
     create_info.pfnUserCallback = debug_callback;
     create_info.pUserData = nullptr; // Optional
 }
