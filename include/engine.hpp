cdc10ae89e7e8c461de5b8ed77cb385354dea329
KillByte <killbyte.prototype@gmail.com>
2021-08-22 01:08:01 +0100

    Added something idk ...

--- include/engine.hpp
+++ include/engine.hpp
@@ -3,6 +3,8 @@
 
 #include "types.hpp"
 
+#include <vector>
+
 namespace kzn
 {
 
@@ -11,11 +13,22 @@ class Engine
 public:
 
     bool _is_initialized{ false };
-    int _frame_number{ 0 };
+    int  _frame_number{ 0 };
 
     VkExtent2D _window_extent{ 1700 , 900 };
 
     struct GLFWwindow* _window{ nullptr };
+
+    VkInstance               _instance; // Vulkan library handle
+    VkDebugUtilsMessengerEXT _debug_messenger; // Vulkan debug output handle
+    VkPhysicalDevice         _physical_device; // GPU chosen as the default device
+    VkDevice                 _device; // Vulkan device for commands
+    VkSurfaceKHR             _surface; // Vulkan window surface
+
+    VkSwapchainKHR _swapchain;
+    VkFormat _swapchain_image_format;
+    std::vector<VkImage> _swapchain_images;
+    std::vector<VkImageView> _swapchain_image_views;
 
 public:
 
@@ -30,6 +43,12 @@ public:
 
     // Run main loop
     void run();
+
+private:
+
+    void init_vulkan();
+
+    void init_swapchain();
 };
 
 }
--- src/engine.cpp
+++ src/engine.cpp
@@ -6,17 +6,19 @@
 #include "types.hpp"
 #include "initializers.hpp"
 
+#include "VkBootstrap.h"
+
 #include <iostream>
 
-#define VK_CHECK(x)                                                    \
-    do                                                                 \
-    {                                                                  \
-        VkResult err = x;                                              \
-        if (err)                                                       \
-        {                                                              \
-            std::cout <<"Vulkan Error: " << err << std::endl; \
-            std::abort();                                                   \
-        }                                                              \
+#define VK_CHECK(x)                                            \
+    do                                                         \
+    {                                                          \
+        VkResult err = x;                                      \
+        if (err)                                               \
+        {                                                      \
+            std::cout << "Vulkan Error: " << err << std::endl; \
+            std::abort();                                      \
+        }                                                      \
     } while (0)
 
 namespace kzn
@@ -38,13 +40,42 @@ void Engine::init()
     // Initialize window
     _window = glfwCreateWindow(_window_extent.width, _window_extent.height, "Kazan Engine", nullptr, nullptr);
 
+    // Create the core Vulkan instances
+    init_vulkan();
+
+    // Create the swapchain
+    init_swapchain();
+
     _is_initialized = true;
 }
+
 
 void Engine::cleanup()
 {
     if(_is_initialized)
     {
+        // Destroy Swapchain
+        vkDestroySwapchainKHR(_device, _swapchain, nullptr);
+
+        // Destroy swapchain resources
+        for (int i = 0; i < _swapchain_image_views.size(); i++)
+        {
+            // Images are implicitly destroyed by the swapchain
+            vkDestroyImageView(_device, _swapchain_image_views[i], nullptr);
+        }
+
+        // Destroy device
+        vkDestroyDevice(_device, nullptr);
+
+        // Destroy surface
+        vkDestroySurfaceKHR(_instance, _surface, nullptr);
+
+        // Destroy Debug messenger
+        vkb::destroy_debug_utils_messenger(_instance, _debug_messenger);
+
+        // Destroy instance
+        vkDestroyInstance(_instance, nullptr);
+
         // Destroy window
         glfwDestroyWindow(_window);
 
@@ -53,10 +84,12 @@ void Engine::cleanup()
     }
 }
 
+
 void Engine::draw()
 {
 
 }
+
 
 void Engine::run()
 {
@@ -66,6 +99,71 @@ void Engine::run()
         glfwPollEvents();
         draw();
     }
+}
+
+
+void Engine::init_vulkan()
+{
+    vkb::InstanceBuilder builder;
+
+    // Make the Vulkan instance, with basic debug features
+    auto inst_ret = builder.set_app_name("Hello triangle")
+        .request_validation_layers(true)
+        .require_api_version(1, 1, 0)
+        .use_default_debug_messenger()
+        .build();
+
+    vkb::Instance vkb_inst = inst_ret.value();
+
+    // Store the instance
+    _instance = vkb_inst.instance;
+    // Store the debug messenger
+    _debug_messenger = vkb_inst.debug_messenger;
+
+
+
+    if(glfwCreateWindowSurface(_instance, _window, nullptr, &_surface) != VK_SUCCESS)
+    {
+        throw std::runtime_error("failed to create window surface!");
+    }
+
+    // We want a GPU that can write to the SDL surface and supports Vulkan 1.1
+    vkb::PhysicalDeviceSelector selector{ vkb_inst };
+    vkb::PhysicalDevice vkb_physical_device = selector
+        .set_minimum_version(1, 1)
+        .set_surface(_surface)
+        .select()
+        .value();
+
+    //create the final Vulkan device
+    vkb::DeviceBuilder vkb_device_builder{ vkb_physical_device };
+
+    vkb::Device vkb_device = vkb_device_builder.build().value();
+
+    // Get the VkDevice handle used in the rest of a Vulkan application
+    _device = vkb_device.device;
+    _physical_device = vkb_physical_device.physical_device;
+}
+
+
+void Engine::init_swapchain()
+{
+    vkb::SwapchainBuilder swapchainBuilder{_physical_device,_device,_surface };
+
+    vkb::Swapchain vkb_swapchain = swapchainBuilder
+        .use_default_format_selection()
+        //use vsync present mode
+        .set_desired_present_mode(VK_PRESENT_MODE_FIFO_KHR)
+        .set_desired_extent(_window_extent.width, _window_extent.height)
+        .build()
+        .value();
+
+    // Store swapchain and its related images
+    _swapchain = vkb_swapchain.swapchain;
+    _swapchain_images = vkb_swapchain.get_images().value();
+    _swapchain_image_views = vkb_swapchain.get_image_views().value();
+
+    _swapchain_image_format = vkb_swapchain.image_format;
 }
 
 }
