027c24555b2854951dc12580ed095326b850820c
KillByte <killbyte.prototype@gmail.com>
2021-10-09 23:51:58 +0100

    General commit

--- include/descriptor.hpp
+++ include/descriptor.hpp
@@ -77,27 +77,25 @@ public:
 };
 
 
-// template<typename T, size_t N>
-// class UniformBufferArray
-// {
-// public:
+template<typename T, size_t N>
+class UniformBufferArray
+{
+public:
 
-//     // friend class Renderer;
+    // friend class Renderer;
 
-//     Device*                                   device = nullptr;
-//     uint32_t*                                 current_index = nullptr;
-//     uint32_t                                  binding;
-//     std::vector<VkBuffer>                     buffers;
-//     std::vector<VkDeviceMemory>               buffers_memory;
+    Device*                                   device = nullptr;
+    uint32_t*                                 current_index = nullptr;
+    uint32_t                                  binding;
+    std::vector<VkBuffer>                     buffers;
+    std::vector<VkDeviceMemory>               buffers_memory;
 
-// public:
+public:
 
-//     // UniformBuffer(const UniformBuffer&) = default;
-//     UniformBufferArray() = default;
-//     ~UniformBufferArray() = default;
+    void cleanup();
 
-//     void update(const std::array<T,N>& data);
-// };
+    void update(const std::array<T,N>& data);
+};
 
 
 template<typename T>
@@ -154,6 +152,8 @@ public:
 
     template<typename T>
     UniformBuffer<T> create_uniform_buffer(VkShaderStageFlags stage = VK_SHADER_STAGE_ALL_GRAPHICS);
+    template<typename T, size_t N>
+    UniformBufferArray<T,N> create_uniform_buffer_array(VkShaderStageFlags stage = VK_SHADER_STAGE_ALL_GRAPHICS);
 
     DescriptorSet build();
 };
@@ -200,14 +200,25 @@ void UniformBuffer<T>::update(const T& data)
 }
 
 
-// template<typename T, size_t N>
-// void UniformBufferArray<T,N>::update(const std::array<T,N>& data)
-// {
-//     void* device_data;
-//     vkMapMemory(device->device(), buffers_memory[*current_index], 0, sizeof(T), 0, &device_data);
-//     memcpy(device_data, &data, sizeof(T)*N);
-//     vkUnmapMemory(device->device(), buffers_memory[*current_index]);
-// }
+template<typename T, size_t N>
+void UniformBufferArray<T,N>::cleanup()
+{
+    for(size_t i = 0; i < buffers.size(); ++i)
+    {
+        vkDestroyBuffer(device->device(), buffers[i], nullptr);
+        vkFreeMemory(device->device(), buffers_memory[i], nullptr);
+    }
+}
+
+
+template<typename T, size_t N>
+void UniformBufferArray<T,N>::update(const std::array<T,N>& data)
+{
+    void* device_data;
+    vkMapMemory(device->device(), buffers_memory[*current_index], 0, sizeof(T), 0, &device_data);
+    memcpy(device_data, &data, sizeof(T)*N);
+    vkUnmapMemory(device->device(), buffers_memory[*current_index]);
+}
 
 
 template<typename T>
@@ -247,12 +258,6 @@ UniformBuffer<T> DescriptorSetBuilder::create_uniform_buffer(VkShaderStageFlags 
         // _uniform_buffers.push_back(ub.buffers[i]);
         // _uniform_buffers_memory.push_back(ub.buffers_memory[i]);
 
-        // VkDescriptorBufferInfo buffer_info{};
-        // buffer_info.buffer = ub.buffers[i];
-        // buffer_info.offset = 0;
-        // buffer_info.range = sizeof(T);
-        // descriptor_buffer_infos.push_back(buffer_info);
-
         descriptor_buffer_infos.push_back(
             VkDescriptorBufferInfo{
                 .buffer = ub.buffers[i],
@@ -260,6 +265,59 @@ UniformBuffer<T> DescriptorSetBuilder::create_uniform_buffer(VkShaderStageFlags 
                 .range  = sizeof(T)
             }
         );
+    }
+
+    layout_bindings.push_back({
+        .binding =            ub.binding,
+        .descriptorType =     VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER,
+        .descriptorCount =    1,
+        .stageFlags =         stage,
+        .pImmutableSamplers = nullptr
+    });
+
+    return ub;
+}
+
+
+template<typename T, size_t N>
+UniformBufferArray<T,N> DescriptorSetBuilder::create_uniform_buffer_array(VkShaderStageFlags stage)
+{
+    UniformBuffer<T> ub;
+    ub.device = this->device;
+    ub.buffers = std::vector<VkBuffer>(image_count);
+    ub.buffers_memory = std::vector<VkDeviceMemory>(image_count);
+    ub.current_index = this->current_index;
+    ub.binding = static_cast<uint32_t>(descriptor_buffer_infos.size()) / image_count;
+    VkDeviceSize buffer_size = sizeof(T)*N;
+
+    for(size_t i = 0; i < image_count; ++i)
+    {
+        device->create_buffer(
+            buffer_size,
+            VK_BUFFER_USAGE_UNIFORM_BUFFER_BIT,
+            VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT, //| VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+            ub.buffers[i],
+            ub.buffers_memory[i]);
+
+        // _uniform_buffers.push_back(ub.buffers[i]);
+        // _uniform_buffers_memory.push_back(ub.buffers_memory[i]);
+
+        // VkDescriptorBufferInfo buffer_info{};
+        // buffer_info.buffer = ub.buffers[i];
+        // buffer_info.offset = 0;
+        // buffer_info.range = sizeof(T);
+        // descriptor_buffer_infos.push_back(buffer_info);
+
+        for(size_t j = 0; j < N; ++j)
+        {
+            descriptor_buffer_infos.push_back(
+                VkDescriptorBufferInfo{
+                    .buffer = ub.buffers[i],
+                    .offset = j*sizeof(T),
+                    .range  = sizeof(T)
+                }
+            );
+        }
     }
 
     layout_bindings.push_back({
