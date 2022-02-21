5a785ed4a1bedd57c7151ba72c6c0d455f420ef7
KillByte <killbyte.prototype@gmail.com>
2022-02-21 22:37:15 +0000

    Updated to kazan static lib generation

--- xmake.lua
+++ xmake.lua
@@ -40,10 +40,10 @@ target("shaders")
     add_files("assets/shaders/**.vert")
     add_files("assets/shaders/**.frag")
 
--------------------- Renderer --------------------
+-------------------- Kazan Lib -------------------
 
--- Target Renderer Executable
-target("renderer")
+-- Target Kazan Static Library
+target("kazan")
     -- Compiler Options
     set_languages("cxx20") -- -std=c++20
     set_warnings("allextra") -- -Wall -Wextra
@@ -56,10 +56,33 @@ target("renderer")
     add_includedirs("include/lib/imgui")
     add_includedirs("include/lib/pegtl")
     add_includedirs("include/lib/tiny_obj_loader")
-    -- Executable
-    set_kind("binary")
+    -- Library
+    set_kind("static")
     add_files("src/**.cpp")
     -- Dependencies
     add_packages("vulkan-headers", {links = "vulkan"})
     add_packages("glfw")
     add_packages("glm")
+
+-------------------- Kazan GUI -------------------
+
+-- target("kazui")
+--     -- Compiler Options
+--     set_languages("cxx20") -- -std=c++20
+--     set_warnings("allextra") -- -Wall -Wextra
+--     set_optimize("fastest") -- -O3
+--     set_targetdir("bin/")
+--     add_links("pthread") -- -lpthread
+--     add_cxxflags("-Wshadow", "-Wfatal-errors", "-Wpedantic")
+--     add_includedirs("include")
+--     add_includedirs("include/lib/stb")
+--     add_includedirs("include/lib/imgui")
+--     add_includedirs("include/lib/pegtl")
+--     add_includedirs("include/lib/tiny_obj_loader")
+--     -- Library
+--     set_kind("binary")
+--     add_files("src/**.cpp")
+--     -- Dependencies
+--     add_packages("vulkan-headers", {links = "vulkan"})
+--     add_packages("glfw")
+--     add_packages("glm")
