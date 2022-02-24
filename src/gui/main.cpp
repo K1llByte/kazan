43375c4b70bd783b5566c5be67051cbc25d9c7ca
KillByte <killbyte.prototype@gmail.com>
2022-02-24 02:31:23 +0000

    Api Perfection

--- src/gui/main.cpp
+++ src/gui/main.cpp
@@ -7,55 +7,52 @@
 using namespace kzn;
 
 
-class Application
-{
-public:
-    Application()
-        : window("Hello World!", 800, 600),
-        instance(vk::InstanceBuilder()
-            .enable_validation_layers()
-            .build()),
-        device(vk::DeviceBuilder(instance)
-            .build())
-    {}
+// class Application
+// {
+// public:
+//     Application()
+//         : window("Hello World!", 800, 600),
+//         instance(vk::InstanceBuilder()
+//             .enable_validation_layers()
+//             .build()),
+//         device(vk::DeviceBuilder(instance)
+//             .build())
+//     {}
 
-    void run()
-    {
-        while(!window.should_close())
-        {
-            window.poll_events();
-        }
-    }
+//     void run()
+//     {
+//         while(!window.should_close())
+//         {
+//             window.poll_events();
+//         }
+//     }
 
-private:
-    Window window;
-    vk::Instance instance;
-    vk::Device device;
-};
+// private:
+//     Window window;
+//     vk::Instance instance;
+//     vk::Device device;
+// };
 
 int main()
 {
     // Log::info("Hello World!");
-    // auto window = Window("Hello World!", 800, 600);
-    // auto instance = vk::InstanceBuilder()
-    //     .enable_validation_layers()
-    //     .build();
+    auto window = Window("Hello World!", 800, 600);
+    auto instance = vk::InstanceBuilder()
+        .enable_validation_layers()
+        .build();
 
-    // auto device = vk::DeviceBuilder(instance)
-    //     .build();
+    auto device = vk::DeviceBuilder(instance)
+        .build();
 
-    // // auto surface = instance.create_surface(window.ptr());
-    // while(!window.should_close())
-    // {
-    //     window.poll_events();
-    // }
+    // auto surface = instance.create_surface(window.ptr());
+    while(!window.should_close())
+    {
+        window.poll_events();
+    }
     // instance.destroy_surface(surface);
 
     // List all devices if you want to choose the device
     // auto available_gpus = vk::Device::available_devices(instance);
     // Create default device
     // auto device = vk::Device::default();
-
-    auto app = Application();
-    app.run();
 }
